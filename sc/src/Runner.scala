0bca24b81796669ff0664c0aa44a2fc9cc4dce52
Denis <denisdenisdenis@bigmir.net>
2017-03-21 23:27:10 +0200

    add line calculator

--- sc/src/Runner.scala
+++ sc/src/Runner.scala
@@ -1,9 +1,12 @@
+import java.io.File
+
 /**
   * Created by Denis on 21-Mar-17.
   */
 object Runner {
   def main(args:Array[String]) {
     println("Hello world")
+    val lineCalculator = new LineCalculator(new File("Runner.scala"))
   }
 
 }
