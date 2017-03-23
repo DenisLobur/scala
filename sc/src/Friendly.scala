039f135742b74e5d523e18fedda8899106964f93
Denis <denisdenisdenis@bigmir.net>
2017-03-23 22:24:29 +0200

    Add mixin

---
+++ sc/src/Friendly.scala
@@ -0,0 +1,6 @@
+/**
+  * Created by Denis on 23-Mar-17.
+  */
+trait Friendly {
+  def greet() = "Hi there"
+}
--- sc/src/Runner.scala
+++ sc/src/Runner.scala
@@ -4,9 +4,17 @@ import java.io.File
   * Created by Denis on 21-Mar-17.
   */
 object Runner {
-  def main(args:Array[String]) {
+  def main(args: Array[String]) {
     println("Hello world")
-    val lineCalculator = new LineCalculator(new File("Runner.scala"))
+    // val lineCalculator = new LineCalculator(new File("Runner.scala"))
+    val one = new RepeatGreater("Hi there")
+    val two = new RepeatGreater("hello", 2)
+    one.greet()
+    two.greet()
+    println()
+
+    val pup:Friendly = new Dog with ExclamatoryDog
+    println(pup.greet())
   }
 
 }
