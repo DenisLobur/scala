8f690acccf043136a21dcc7b329e882eced2d19b
denislobur <denisdenisdenis@bigmir.net>
2017-04-11 19:52:48 +0300

    add another way to pass parameter to function

--- src/main/scala/chapter_9/Currying.scala
+++ src/main/scala/chapter_9/Currying.scala
@@ -1,5 +1,8 @@
 package chapter_9
 
+import java.io.{File, PrintWriter}
+import java.util.Date
+
 /**
   * Created by denys on 4/11/17.
   */
@@ -10,8 +13,28 @@ object Currying {
   //Currying way of presenting function
   def sumCurryingWay(a: Int)(b: Int) = a + b
 
-  sumOldWay(1,3)
+  sumOldWay(1, 3)
   sumCurryingWay(2)(3)
 
   // fun(x,y) = fun(x)(y)
+
+  //Substituting two parameter lists with one arg instead of one parameter list with two args
+  def withPrintWriter(file: File)(op: PrintWriter => Unit) = {
+    val writer = new PrintWriter(file)
+    try {
+      op(writer)
+    } finally {
+      writer.close()
+    }
+  }
+
+  // ^ is equivalent to v
+
+  // the first argument list, which contains one File argument, is
+  // written surrounded by parentheses. The second argument list, which contains
+  // one function argument, is surrounded by curly braces
+  val file = new File("date.txt")
+  withPrintWriter(file) {
+    writer => writer.println(new java.util.Date)
+  }
 }
